05494b43f8289c1cbfa2f153e62b9be6e73cdcf1
Steven Decock <decocksteven79@gmail.com>
2018-04-17 11:44:19 +0200

    [Steven] Exercise 10: Remove solution

--- exercises/src/main/java/com/switchfully/vaadin/exercise_10_keyboard_shortcuts/ui/ExampleForm1.java
+++ exercises/src/main/java/com/switchfully/vaadin/exercise_10_keyboard_shortcuts/ui/ExampleForm1.java
@@ -1,22 +1,10 @@
 package com.switchfully.vaadin.exercise_10_keyboard_shortcuts.ui;
 
-import com.vaadin.event.Action;
 import com.vaadin.event.ShortcutAction;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
 
-public class ExampleForm1 extends CustomComponent
-        /* Solution Added Start */
-        implements Action.Handler
-        /* Solution Added End */
-{
-    // Solution Added Start
-    Action action_ok = new ShortcutAction("Default key",
-            ShortcutAction.KeyCode.ENTER, null);
-
-    Action action_clear = new ShortcutAction("Ctrl+C",
-            ShortcutAction.KeyCode.C, new int[] { ShortcutAction.ModifierKey.CTRL });
-    // Solution Added End
+public class ExampleForm1 extends CustomComponent {
 
     private Panel panel;
     private final Button searchButton;
@@ -25,25 +13,21 @@ public class ExampleForm1 extends CustomComponent
     private final Label result;
 
     public ExampleForm1() {
+        // TODO Exercise 10: Fix this form to only handle key shortcuts in context of this form.
         panel = new Panel("Search form");
-        panel.addActionHandler(this);
 
         result = new Label();
 
         textField = new TextField("Filter");
         searchButton = new Button("Search", e -> result.setValue("Searching with filter " + textField.getValue()));
         searchButton.addStyleName(ValoTheme.BUTTON_PRIMARY);
-        // Solution removed Start
-//        searchButton.setClickShortcut(ShortcutAction.KeyCode.ENTER);
-        // Solution removed End
+        searchButton.setClickShortcut(ShortcutAction.KeyCode.ENTER);
 
         clearButton = new Button("Clear", e -> {
             textField.clear();
             result.setValue("");
         });
-        // Solution removed Start
-//        clearButton.setClickShortcut(ShortcutAction.KeyCode.C, new int[] { ShortcutAction.ModifierKey.CTRL });
-        // Solution removed End
+        clearButton.setClickShortcut(ShortcutAction.KeyCode.C, new int[] { ShortcutAction.ModifierKey.CTRL });
 
         HorizontalLayout buttons = new HorizontalLayout(searchButton, clearButton);
         buttons.setSpacing(true);
@@ -52,22 +36,5 @@ public class ExampleForm1 extends CustomComponent
 
         setCompositionRoot(panel);
     }
-
-    /* Solution Added Start */
-    @Override
-    public Action[] getActions(Object target, Object sender) {
-        return new Action[] {action_ok, action_clear};
-    }
-
-    @Override
-    public void handleAction(Action action, Object sender, Object target) {
-        if (action == action_ok) {
-            searchButton.click();
-        }
-        if (action == action_clear) {
-            clearButton.click();
-        }
-    }
-    /* Solution Added End */
 
 }
