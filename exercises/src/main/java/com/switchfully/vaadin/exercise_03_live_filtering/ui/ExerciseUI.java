6028c2494bff2aa028ba3f101b9fa5551b610a7f
reneald <delea.vanreeth@gmail.com>
2018-04-20 13:43:10 +0200

    Solution to Exercise 3

--- exercises/src/main/java/com/switchfully/vaadin/exercise_03_live_filtering/ui/ExerciseUI.java
+++ exercises/src/main/java/com/switchfully/vaadin/exercise_03_live_filtering/ui/ExerciseUI.java
@@ -3,20 +3,14 @@ package com.switchfully.vaadin.exercise_03_live_filtering.ui;
 import com.switchfully.vaadin.domain.Accomodation;
 import com.switchfully.vaadin.service.AccomodationService;
 import com.vaadin.annotations.Theme;
-import com.vaadin.data.Property;
 import com.vaadin.data.util.BeanItemContainer;
-import com.vaadin.event.FieldEvents;
+import com.vaadin.data.util.filter.SimpleStringFilter;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.toList;
 
 @SpringUI
 @Theme("valo")
@@ -33,7 +27,6 @@ public class ExerciseUI extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
-        VerticalLayout mainLayout = new VerticalLayout(grid);
 
         BeanItemContainer<Accomodation> container =
                 new BeanItemContainer<>(Accomodation.class, accomodationService.getAccomodations());
@@ -46,9 +39,29 @@ public class ExerciseUI extends UI {
 
         grid.setContainerDataSource(container);
 
-        // TODO Exercise 3: Add a filter TextField to the top of the grid to filter the list of accomodations by name.
-        // TODO Exercise 3: Add a button next to the filter TextField to clear the filter.
+        TextField filterField = new TextField();
+        filterField.setInputPrompt("Filter by name...");
+        filterField.addTextChangeListener(change -> {
+            container.removeAllContainerFilters();
+            if (!change.getText().isEmpty()) {
+                container.addContainerFilter(
+                        new SimpleStringFilter("name", change.getText(), true, false)
+                );
+            }
+        });
 
+        Button clearFilter = new Button(FontAwesome.TIMES);
+        clearFilter.setDescription("Clear the current filter");
+        clearFilter.addClickListener(click -> {
+            filterField.clear();
+            container.removeAllContainerFilters();
+        });
+
+        CssLayout filtering = new CssLayout();
+        filtering.addComponents(filterField, clearFilter);
+        filtering.setStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+
+        VerticalLayout mainLayout = new VerticalLayout(filtering, grid);
         mainLayout.setMargin(true);
         setContent(mainLayout);
     }
