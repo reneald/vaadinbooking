b399d71fb2b93f6f47061090999e3940fa1054b8
reneald <delea.vanreeth@gmail.com>
2018-04-20 14:06:55 +0200

    Solution to Exercise 4

--- exercises/src/main/java/com/switchfully/vaadin/exercise_04_field_binding_simple/ui/ExerciseUI.java
+++ exercises/src/main/java/com/switchfully/vaadin/exercise_04_field_binding_simple/ui/ExerciseUI.java
@@ -3,14 +3,9 @@ package com.switchfully.vaadin.exercise_04_field_binding_simple.ui;
 import com.vaadin.annotations.Theme;
 import com.vaadin.data.Property;
 import com.vaadin.data.util.ObjectProperty;
-import com.vaadin.event.FieldEvents;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import static com.vaadin.ui.AbstractTextField.TextChangeEventMode.EAGER;
-import static com.vaadin.ui.AbstractTextField.TextChangeEventMode.LAZY;
 
 @SpringUI
 @Theme("valo")
@@ -18,13 +13,19 @@ public class ExerciseUI extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
-        // TODO Exercise 4: Create a TextField and a Label, both bound to the same 'name' Property.
-        // TODO Exercise 4: Add a button to commit the field.
-        // TODO Exercise 4: Clicking the button should update the Label with the value in the TextField.
+        Property<String> name = new ObjectProperty<>("name");
+        TextField input = new TextField("input", name);
+        Label output = new Label(name);
+        CheckBox autoCommit = new CheckBox("auto commit");
+        Button commit = new Button("Commit input");
 
-        // TODO Exercise 4 (Extra): Add a checkbox to hide the button and make the TextField auto-commit.
+        autoCommit.addValueChangeListener(valueChangeEvent -> {
+            input.setImmediate(autoCommit.getValue());
+            commit.setVisible(!autoCommit.getValue());
+        });
+        commit.addClickListener(clickEvent -> name.setValue(input.getValue()));
 
-        VerticalLayout mainLayout = new VerticalLayout();
+        VerticalLayout mainLayout = new VerticalLayout(input, output, autoCommit, commit);
         mainLayout.setMargin(true);
         setContent(mainLayout);
     }
